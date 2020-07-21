0dffe6e1eaa9569a5106438aa9e562c315653093
root <root@5e9b4617fd56>
2020-07-21 23:49:19 +0000

    Unbirthday

---
+++ src/_03_if_else/_1_unbirthday/Unbirthday.java
@@ -0,0 +1,34 @@
+package _03_if_else._1_unbirthday;
+
+import javax.swing.JOptionPane;
+
+public class Unbirthday {
+public static void main(String[] args) {
+	
+	// String input = JOptionPane.showInputDialog("When is your birthday?");
+	String input = JOptionPane.showInputDialog("When is your birthday?");
+	
+	
+	// if(input.equals()){
+	//
+	if (input.equals("07/21/2020")){
+		//JoptionPaneshowM?
+	JOptionPane.showMessageDialog(null, "Happy Birthday!");
+	} 
+	else if (input.equals("03/03/2016")){
+	JOptionPane.showMessageDialog(null, "Happy birthday!!!");
+	}
+//	else {
+//		JOptionPane.showMessageDialog(null, "Happy unbirthday!!");
+//	}
+	
+	else {
+		JOptionPane.showMessageDialog(null, "Very merry unbirthday!!");
+	}
+	
+	
+	// Si se entiende? Intenta escribir el else
+	
+	
+}
+}
